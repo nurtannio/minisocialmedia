6476858aec8b9251292ba95a34a52bf3467fc1ce
nurtannio <mnurtannio25@gmail.com>
2017-12-17 13:07:16 +0700

    profile view

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('username', 32);
-            $table->date('dob');
+            $table->date('dob');  
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
